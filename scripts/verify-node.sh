7679513a87e18ef027ae244f36cf2d6220d4e034
balamir <acimseda2@gmail.com>
2026-06-19 23:49:25 +0300

    Create verify-node.sh

---
+++ scripts/verify-node.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Execution Layer Version:"
+arc-node-execution --version
+
+echo ""
+
+echo "Consensus Layer Version:"
+arc-node-consensus --version
+
+echo ""
+
+echo "Snapshots Version:"
+arc-snapshots --version
